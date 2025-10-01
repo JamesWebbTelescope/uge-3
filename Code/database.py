3da84dedc76231d9e46daba29ae6932e5da75fe0
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-01 15:05:01 +0200

    database.py now includes an app.

--- Code/database.py
+++ Code/database.py
@@ -1,4 +1,7 @@
 import mysql.connector
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
 
 class database_class:
     def __new__(db_a):
@@ -66,8 +69,55 @@ class database_class:
 
 database = database_class()
 
-database.open_connection()
+app = Flask(__name__)
+# creating an API object
+api = Api(app)
+
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # is a GET request for this resource
+    def get(self):
+
+        return jsonify({'message': 'hello world'})
+
+    # Corresponds to POST request
+    def post(self):
+        
+        data = request.get_json()     # status code
+        return jsonify({'data': data}), 201
+
+
+# another resource to calculate the square of a number
+class Square(Resource):
+
+    def get(self, num):
+
+        return jsonify({'square': num**2})
+    
+class Requests(Resource):
+    type = "None"
+
+    @staticmethod
+    def create(request_type):
+        if request_type == "Update":
+            type = "Request"
 
 
 
+
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+
+
+# driver function
+if __name__ == '__main__':
+
+    app.run(debug = True)
 
